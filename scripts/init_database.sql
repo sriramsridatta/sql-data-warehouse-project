06e0c7cd1bc32ec47dfc0e4ff8047d7dbadbc8e8
Sriram Sri Datta <sriramsridatta5@gmail.com>
2025-12-04 21:35:50 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,21 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+    
+    In postgress database needs to be connected manually
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
